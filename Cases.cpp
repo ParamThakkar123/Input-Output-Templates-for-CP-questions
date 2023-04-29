9d57400b0da7cdf4d2a691a5ced3c71fb7919a82
Param Thakkar <128291516+ParamThakkar123@users.noreply.github.com>
2023-04-29 19:42:01 +0530

    Create Cases.cpp

---
+++ Cases.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+  int T;
+  cin >> T; // Test Case variable
+  for(int i = 1; i <= T; i++){
+    cin >> var1 >> var2 //variables that you declare (it can be more than 2 modify accordingly)
+    cout << "Case " << i << " : " << result << endl; 
+  }   
+  return 0;
+}
